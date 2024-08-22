6be04d26e6ba7a235804a335d39105abca65e9eb
freedownloadhere <freedownloadhere13@gmail.com>
2024-08-22 15:59:26 +0300

    Initial commit - Episode 1

---
+++ src/main/java/com/github/freedownloadhere/how2mod/HowToMod.java
@@ -0,0 +1,12 @@
+package com.github.freedownloadhere.how2mod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "howmod", useMetadata=true)
+public class HowToMod {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
